59694d81ac46ac7d2c50ef932f77b9ee24c3b21e
DanielRistoff <daniel_ristoff@hotmail.com>
2021-09-02 20:36:02 -0300

    Create delete by id

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,10 +3,14 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -20,12 +24,34 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(PersonDTO personDTO){
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToDate = personMapper.toModel(personDTO);
         Person savePerson = personRepository.save(personToDate);
         return MessageResponseDTO
                 .builder()
-                .message(String.format("Create person with ID %s",savePerson.getId()))
+                .message(String.format("Create person with ID %s", savePerson.getId()))
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = verifiyExistis(id);
+        return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifiyExistis(id);
+        personRepository.deleteById(id);
+    }
+
+    private Person verifiyExistis(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() ->new PersonNotFoundException(id));
     }
 }
